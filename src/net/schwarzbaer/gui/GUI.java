f88c8a91b6aa543ff35f234eba370cb0b054a7f6
Hendrik Scholtz <hendrik.github@nurfuerspam.de>
2011-05-16 20:35:35 +0000

    JAVA: [Eclipse] - Projekt "CommonLib" aktualisiert
    git-svn-id: file:///G/_svn_repo/programming/JAVA-Projects%20-%20Eclipse/CommonLib@993 8592820e-bdd2-cc16-cb4b-ca06df7e8abd

--- src/net/schwarzbaer/gui/GUI.java
+++ src/net/schwarzbaer/gui/GUI.java
@@ -7,6 +7,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
+
+import javax.swing.AbstractButton;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
@@ -24,6 +27,11 @@ import javax.swing.KeyStroke;
  * @author Hendrik
  */
 public class GUI {
+
+    public static void addButtonToPanelAndButtonGroup( JPanel buttonPanel, ButtonGroup buttonGroup, AbstractButton btn ) {
+    	buttonPanel.add(btn);
+    	buttonGroup.add(btn);
+    }
 
     public static void addLabelAndField( JPanel labelPanel, JPanel fieldPanel, String label, JComponent field ) {
         labelPanel.add( new JLabel(label) );
