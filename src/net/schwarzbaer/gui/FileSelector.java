2d2df39b9c8741648de79986512bc82fb15d6868
Hendrik Scholtz <hendrik.github@nurfuerspam.de>
2012-11-21 20:09:11 +0000

    JAVA: [Eclipse] - Projekt "CommonLib"    - FileSelector erzeugt
    git-svn-id: file:///G/_svn_repo/programming/JAVA-Projects%20-%20Eclipse/CommonLib@1552 8592820e-bdd2-cc16-cb4b-ca06df7e8abd

---
+++ src/net/schwarzbaer/gui/FileSelector.java
@@ -0,0 +1,95 @@
+package net.schwarzbaer.gui;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JTextField;
+
+
+public class FileSelector implements ActionListener {
+
+	private JFileChooser fileChooser;
+	private JButton button;
+	private JTextField field;
+	private final Component parent;
+	private final FileSelectorListener listener;
+	private final String id;
+
+	public FileSelector(Component parent, String id, FileSelectorListener listener) {
+		this.parent = parent;
+		this.id = id;
+		this.listener = listener;
+		button = null;
+		fileChooser = new JFileChooser();
+		fileChooser.setMultiSelectionEnabled(false);
+	}
+	
+	public void setCurrentDirectory(String dir) {
+		fileChooser.setCurrentDirectory(new File(dir));
+	}
+
+	public Component getInputField() {
+		if (field==null) field = GUI.createTextField("select field", this, true, null);
+		return field;
+	}
+
+	public Component getSelectButton(String title) {
+		if (button==null) button = GUI.createButton(title, "select button", this, GUI.getFileIcon(new File(".")));
+		return button;
+	}
+
+	public void setDirSelectionOnly() {
+		fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+	}
+
+	public void setFileSelectionOnly() {
+		fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+	}
+
+	public void setFileAndDirSelection() {
+		fileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getActionCommand().equals("select button")) {
+			if (fileChooser.showOpenDialog(parent) == JFileChooser.APPROVE_OPTION) {
+				File selectedFile = fileChooser.getSelectedFile();
+				field.setText(selectedFile.toString());
+				if (checkDir(selectedFile))
+					listener.fileSelectionChanged(id, selectedFile);
+			}
+			return;
+		}
+		if (e.getActionCommand().equals("select field")) {
+			File selectedFile = new File(field.getText());
+			if (checkDir(selectedFile) && listener.isFileANewChoise(id, selectedFile)) {
+				listener.fileSelectionChanged(id, selectedFile);
+				fileChooser.setSelectedFile(selectedFile);
+			}
+			return;
+		}
+	}
+	
+	private boolean checkDir(File selectedFile) {
+		
+		if (!listener.isFileOK(id, selectedFile)) {
+			field.setBackground(Color.RED);
+			return false;
+		}
+		
+		field.setBackground(new JTextField().getBackground());
+		return true;
+	}
+	
+	public interface FileSelectorListener {
+		public void    fileSelectionChanged(String id, File file);
+		public boolean isFileOK            (String id, File file);
+		public boolean isFileANewChoise    (String id, File file);
+	}
+}
