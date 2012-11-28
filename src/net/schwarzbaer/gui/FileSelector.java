def3167f957ea94643768dfccdeebd67172dda09
Hendrik Scholtz <hendrik.github@nurfuerspam.de>
2012-11-28 19:16:22 +0000

    JAVA: [Eclipse] - Projekt "CommonLib"
    git-svn-id: file:///G/_svn_repo/programming/JAVA-Projects%20-%20Eclipse/CommonLib@1576 8592820e-bdd2-cc16-cb4b-ca06df7e8abd

--- src/net/schwarzbaer/gui/FileSelector.java
+++ src/net/schwarzbaer/gui/FileSelector.java
@@ -6,28 +6,52 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.util.Iterator;
+import java.util.Vector;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
 import javax.swing.JTextField;
+import javax.swing.MutableComboBoxModel;
+import javax.swing.event.ListDataEvent;
+import javax.swing.event.ListDataListener;
 
 
 public class FileSelector implements ActionListener {
 
+	private final Component parent;
+	private final String id;
+	private final Type type;
+	private final FileSelectorListener listener;
 	private JFileChooser fileChooser;
 	private JButton button;
 	private JTextField field;
-	private final Component parent;
-	private final FileSelectorListener listener;
-	private final String id;
+	private JComboBox<String> cmbbx;
+	private Color defaultFieldBackground;
 
 	public FileSelector(Component parent, String id, FileSelectorListener listener) {
+		this(parent, id, Type.WITHOUT_ALTERNATIVES, listener);
+	}
+	
+	public FileSelector(Component parent, String id, Type type, FileSelectorListener listener) {
 		this.parent = parent;
 		this.id = id;
+		this.type = type;
 		this.listener = listener;
-		button = null;
-		fileChooser = new JFileChooser();
+		fileChooser = new JFileChooser(".");
 		fileChooser.setMultiSelectionEnabled(false);
+		button = null;
+		field = null;
+		cmbbx = null;
+		switch(type) {
+		case WITH_ALTERNATIVES   :
+			JComboBox<String> comboBox = new JComboBox<String>();
+			comboBox.setEditable(true);
+			defaultFieldBackground = comboBox.getBackground();
+			break;
+		case WITHOUT_ALTERNATIVES: defaultFieldBackground = new JTextField().getBackground(); break;
+		}
+		
 	}
 	
 	public void setCurrentDirectory(String dir) {
@@ -35,23 +59,41 @@ public class FileSelector implements ActionListener {
 	}
 
 	public void addAlternative(String dir) {
-		// TODO Auto-generated method stub
+		System.out.printf("addAlternative(\"%s\")\r\n",dir);
+		if (type==Type.WITHOUT_ALTERNATIVES) return;
+		cmbbx.addItem(dir);
 		
 	}
 
 	public Iterator<String> getAlternatives() {
+		if (type==Type.WITHOUT_ALTERNATIVES) return null;
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	public void setEnabled(boolean b) {
 		button.setEnabled(b);
-		field .setEnabled(b);
+		switch(type) {
+		case WITH_ALTERNATIVES   : cmbbx .setEnabled(b); break;
+		case WITHOUT_ALTERNATIVES: field .setEnabled(b); break;
+		}
 	}
 
 	public Component getInputField() {
-		if (field==null) field = GUI.createTextField("select field", this, true, null);
-		return field;
+		switch(type) {
+		case WITH_ALTERNATIVES:
+			if (cmbbx==null) {
+				cmbbx = new JComboBox<String>(new AlternativeModel());
+				cmbbx.setActionCommand("select field");
+				cmbbx.addActionListener(this);
+				cmbbx.setEditable(true);
+			}
+			return cmbbx;
+		case WITHOUT_ALTERNATIVES:
+			if (field==null) field = GUI.createTextField("select field", this, true, null);
+			return field;
+		}
+		return null;
 	}
 
 	public Component getSelectButton(String title) {
@@ -59,31 +101,23 @@ public class FileSelector implements ActionListener {
 		return button;
 	}
 
-	public void setDirSelectionOnly() {
-		fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-	}
-
-	public void setFileSelectionOnly() {
-		fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-	}
-
-	public void setFileAndDirSelection() {
-		fileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-	}
+	public void setDirSelectionOnly   () { fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY); }
+	public void setFileSelectionOnly  () { fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY); }
+	public void setFileAndDirSelection() { fileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES); }
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if (e.getActionCommand().equals("select button")) {
 			if (fileChooser.showOpenDialog(parent) == JFileChooser.APPROVE_OPTION) {
 				File selectedFile = fileChooser.getSelectedFile();
-				field.setText(selectedFile.toString());
+				setFieldText(selectedFile.toString());
 				if (checkDir(selectedFile))
 					listener.fileSelectionChanged(id, selectedFile);
 			}
 			return;
 		}
 		if (e.getActionCommand().equals("select field")) {
-			File selectedFile = new File(field.getText());
+			File selectedFile = new File(getFieldText());
 			if (checkDir(selectedFile) && listener.isFileANewChoice(id, selectedFile)) {
 				listener.fileSelectionChanged(id, selectedFile);
 				fileChooser.setSelectedFile(selectedFile);
@@ -91,18 +125,115 @@ public class FileSelector implements ActionListener {
 			return;
 		}
 	}
+
+	private String getFieldText() {
+		switch(type) {
+		case WITH_ALTERNATIVES   : return cmbbx.getSelectedItem().toString();
+		case WITHOUT_ALTERNATIVES: return field.getText();
+		}
+		return null;
+	}
+	
+	private void setFieldText(String txt) {
+		switch(type) {
+		case WITH_ALTERNATIVES   : cmbbx.setSelectedItem(txt); break;
+		case WITHOUT_ALTERNATIVES: field.setText(txt); break;
+		}
+	}
 	
 	private boolean checkDir(File selectedFile) {
 		
 		if (!listener.isFileOK(id, selectedFile)) {
-			field.setBackground(Color.RED);
+			switch(type) {
+			case WITH_ALTERNATIVES   : cmbbx.setBackground(Color.RED); break;
+			case WITHOUT_ALTERNATIVES: field.setBackground(Color.RED); break;
+			}
 			return false;
 		}
 		
-		field.setBackground(new JTextField().getBackground());
+		switch(type) {
+		case WITH_ALTERNATIVES   : cmbbx.setBackground(defaultFieldBackground); break;
+		case WITHOUT_ALTERNATIVES: field.setBackground(defaultFieldBackground); break;
+		}
 		return true;
 	}
 	
+	public static enum Type {
+		WITH_ALTERNATIVES,
+		WITHOUT_ALTERNATIVES
+	}
+	
+	public class AlternativeModel implements MutableComboBoxModel<String> {
+		
+		private final Vector<ListDataListener> listeners;
+		private final Vector<String> items;
+		private String selectedItem;
+
+		public AlternativeModel() {
+			listeners = new Vector<ListDataListener>();
+			items = new Vector<String>();
+		}
+		
+		@Override public void    addListDataListener(ListDataListener ldl) { listeners.   add(ldl); }
+		@Override public void removeListDataListener(ListDataListener ldl) { listeners.remove(ldl); }
+		
+		private void fireContentsChanged(ListDataEvent e) {
+			Iterator<ListDataListener> it = listeners.iterator();
+			while(it.hasNext()) it.next().contentsChanged(e);
+		}
+		private void fireIntervalAdded(ListDataEvent e) {
+			Iterator<ListDataListener> it = listeners.iterator();
+			while(it.hasNext()) it.next().intervalAdded(e);
+		}
+		private void fireIntervalRemoved(ListDataEvent e) {
+			Iterator<ListDataListener> it = listeners.iterator();
+			while(it.hasNext()) it.next().intervalRemoved(e);
+		}
+
+		@Override public int getSize() { return items.size(); }
+		@Override public String getElementAt(int i) {
+			System.out.printf("getElementAt(%d) [=\"%s\"]\r\n",i,items.get(i));
+			return items.get(i);
+		}
+
+		@Override
+		public Object getSelectedItem() {
+			System.out.printf("getSelectedItem() [=\"%s\"]\r\n",this.selectedItem);
+			return this.selectedItem;
+		}
+	
+		@Override
+		public void setSelectedItem(Object selectedItem) {
+			this.selectedItem = (selectedItem==null?null:selectedItem.toString());
+			System.out.printf("setSelectedItem(\"%s\")\r\n",selectedItem);
+		}
+
+		@Override
+		public void addElement(String arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void insertElementAt(String arg0, int arg1) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void removeElement(Object arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void removeElementAt(int arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+	
+	}
+
 	public interface FileSelectorListener {
 		public void    fileSelectionChanged(String id, File file);
 		public boolean isFileOK            (String id, File file);
