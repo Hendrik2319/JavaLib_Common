092e41c959f25fe164afe59fcdb268939e00b7b6
Hendrik Scholtz <hendrik.github@nurfuerspam.de>
2013-07-30 22:36:02 +0000

    JAVA: [Eclipse] - Projekt "CommonLib"
    git-svn-id: file:///G/_svn_repo/programming/JAVA-Projects%20-%20Eclipse/CommonLib@1833 8592820e-bdd2-cc16-cb4b-ca06df7e8abd

--- src/net/schwarzbaer/gui/GUI.java
+++ src/net/schwarzbaer/gui/GUI.java
@@ -6,6 +6,7 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
@@ -28,6 +29,7 @@ import javax.swing.JCheckBox;
 import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JComboBox;
 import javax.swing.JComponent;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
@@ -58,6 +60,15 @@ public final class GUI {
 		catch (InstantiationException e) {}
 		catch (IllegalAccessException e) {}
 		catch (UnsupportedLookAndFeelException e) {}
+	}
+	
+    public static void moveToScreenCenter(JFrame window) {
+    	Dimension size = window.getSize();
+    	Rectangle screen = window.getGraphicsConfiguration().getBounds();
+    	window.setLocation(
+	            (screen.width -size.width )/2+screen.x,
+	            (screen.height-size.height)/2+screen.y
+	        );
 	}
 	
     public static Icon getFileIcon(File file) {
