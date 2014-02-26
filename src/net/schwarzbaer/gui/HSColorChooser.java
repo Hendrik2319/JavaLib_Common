30add632e46286c618b4d4302d226611b5cf0b30
Hendrik Scholtz <hendrik.github@nurfuerspam.de>
2014-02-26 18:05:09 +0000

    JAVA: [Eclipse] - Projekt "CommonLib"    - HSColorChooser um eine Merkliste erweitert
    git-svn-id: file:///G/_svn_repo/programming/JAVA-Projects%20-%20Eclipse/CommonLib@2044 8592820e-bdd2-cc16-cb4b-ca06df7e8abd

--- src/net/schwarzbaer/gui/HSColorChooser.java
+++ src/net/schwarzbaer/gui/HSColorChooser.java
@@ -3,33 +3,36 @@ package net.schwarzbaer.gui;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.GridLayout;
+import java.awt.Rectangle;
 import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
+import javax.swing.Icon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JToggleButton;
 
 import net.schwarzbaer.gui.ColorSlider.ColorChangeListener;
 import net.schwarzbaer.gui.ColorSlider.SliderType;
 
 
 public final class HSColorChooser implements ActionListener, ColorChangeListener {
-
-/**
- *
- * @author hscholtz
- */
 	
 	public static final StandardDialog.Position PARENT_CENTER   = StandardDialog.Position.PARENT_CENTER;
 	public static final StandardDialog.Position LEFT_OF_PARENT  = StandardDialog.Position.LEFT_OF_PARENT;
 	public static final StandardDialog.Position ABOVE_PARENT    = StandardDialog.Position.ABOVE_PARENT;
 	public static final StandardDialog.Position RIGHT_OF_PARENT = StandardDialog.Position.RIGHT_OF_PARENT;
 	public static final StandardDialog.Position BELOW_PARENT    = StandardDialog.Position.BELOW_PARENT;
+	
+	private static Color[] userdefinedColors = new Color[] { null,null,null,null,null,null,null,null }; 
+	private ColorButton[] userColorButtons; 
 	
 	public static Color showDialog(Window parent, String title, Color color, StandardDialog.Position position) {
 		HSColorChooser dialog = new HSColorChooser();
@@ -97,14 +100,14 @@ public final class HSColorChooser implements ActionListener, ColorChangeListener
 		singleSliderPanel.add(rgbPanel);
 		singleSliderPanel.add(hsvPanel);
 		
-		ButtonGroup buttonGroup = new ButtonGroup();
+		ButtonGroup buttonGroup_panelType = new ButtonGroup();
 		JPanel dualTypePanel = new JPanel(new GridLayout(1,0,3,3));
-		dualTypePanel.add(GUI.createRadioButton("RG", "dualRG", this, buttonGroup, true, true));
-		dualTypePanel.add(GUI.createRadioButton("GB", "dualGB", this, buttonGroup, true, true));
-		dualTypePanel.add(GUI.createRadioButton("RB", "dualRB", this, buttonGroup, true, true));
-		dualTypePanel.add(GUI.createRadioButton("HS", "dualHS", this, buttonGroup, true, true));
-		dualTypePanel.add(GUI.createRadioButton("SB", "dualSB", this, buttonGroup, true, true));
-		dualTypePanel.add(GUI.createRadioButton("HB", "dualHB", this, buttonGroup, true, true));
+		dualTypePanel.add(GUI.createRadioButton("RG", "dualRG", this, buttonGroup_panelType, true, true));
+		dualTypePanel.add(GUI.createRadioButton("GB", "dualGB", this, buttonGroup_panelType, true, true));
+		dualTypePanel.add(GUI.createRadioButton("RB", "dualRB", this, buttonGroup_panelType, true, true));
+		dualTypePanel.add(GUI.createRadioButton("HS", "dualHS", this, buttonGroup_panelType, true, true));
+		dualTypePanel.add(GUI.createRadioButton("SB", "dualSB", this, buttonGroup_panelType, true, true));
+		dualTypePanel.add(GUI.createRadioButton("HB", "dualHB", this, buttonGroup_panelType, true, true));
 		
 		JPanel dualSliderPanel = new JPanel(new BorderLayout(3,3));
 //		dualSliderPanel.setBorder(BorderFactory.createTitledBorder(""));
@@ -120,14 +123,36 @@ public final class HSColorChooser implements ActionListener, ColorChangeListener
 		newColorField.setPreferredSize(new Dimension(30,15));
 		oldColorField.setPreferredSize(new Dimension(30,15));
 		
+		userColorButtons = new ColorButton[userdefinedColors.length];
+		ButtonGroup buttonGroup_colorList = new ButtonGroup();
+		JPanel userColorListPanel = new JPanel(new GridLayout(1,0,3,3));
+		//userColorListPanel.setBorder(BorderFactory.createLoweredBevelBorder());
+		for (int i=0; i<userdefinedColors.length; i++) {
+			userColorButtons[i] = new ColorButton(userdefinedColors[i]);
+			buttonGroup_colorList.add(userColorButtons[i]);
+			userColorListPanel.add(userColorButtons[i]);
+		}
+		
+		JPanel userColorButtonPanel = new JPanel(new GridLayout(1,0,3,3));
+		userColorButtonPanel.add(GUI.createButton("set", "set user color", this));
+		userColorButtonPanel.add(GUI.createButton("read", "read user color", this));
+		
+		JPanel userColorPanel = GUI.createLeftAlignedPanel(userColorButtonPanel,userColorListPanel,3);
+		userColorPanel.setBorder(BorderFactory.createEtchedBorder());
+		
+		JPanel lowerPanel = new JPanel(new GridLayout(0,1,3,3));
+		lowerPanel.add(GUI.createRightAlignedPanel(buttonPanel,examplePanel));
+		lowerPanel.add(userColorPanel);
+		
 		JPanel contentPane = new JPanel(new BorderLayout(3,3));
 		contentPane.setBorder(BorderFactory.createEmptyBorder(3,3,3,3));
 		contentPane.add(dualSliderPanel,BorderLayout.CENTER);
 		contentPane.add(singleSliderPanel,BorderLayout.EAST);
-		contentPane.add(GUI.createRightAlignedPanel(buttonPanel,examplePanel),BorderLayout.SOUTH);
+		contentPane.add(lowerPanel,BorderLayout.SOUTH);
 		
 		dlgFenster = new StandardDialog(parent, title);
 		dlgFenster.createGUI(contentPane);
+		dlgFenster.setSizeAsMinSize();
 	}
 
 	private JPanel createSliderPanel(String title, ColorCompSlider colorSlider) {
@@ -150,8 +175,25 @@ public final class HSColorChooser implements ActionListener, ColorChangeListener
 			dlgFenster.closeDialog();
 			return;
 		}
+		
 		if ("zurücksetzen".equals(e.getActionCommand())) {
 			setColor(oldColor);
+			return;
+		}
+		if ("set user color".equals(e.getActionCommand())) {
+			int i = getSelectedColorButton();
+			if (i>=0) {
+				userdefinedColors[i] = newColor;
+				userColorButtons[i].setColor(newColor);
+			}
+			return;
+		}
+		if ("read user color".equals(e.getActionCommand())) {
+			int i = getSelectedColorButton();
+			if ( (i>=0) && (userdefinedColors[i]!=null) ) {
+				setColor(userdefinedColors[i]);
+				userColorButtons[i].setColor(newColor);
+			}
 			return;
 		}
 		if (e.getActionCommand().startsWith("dual")) {
@@ -166,6 +208,13 @@ public final class HSColorChooser implements ActionListener, ColorChangeListener
 		}
 	}
 
+	private int getSelectedColorButton() {
+		for (int i=0; i<userColorButtons.length; i++) {
+			if (userColorButtons[i].isSelected()) return i;
+		}
+		return -1;
+	}
+
 	@Override
 	public void colorChanged(Color color, float f) {
 		setColor(color);
@@ -175,5 +224,71 @@ public final class HSColorChooser implements ActionListener, ColorChangeListener
 	public void colorChanged(Color color, float fH, float fV) {
 		setColor(color);
 	}
+	
+	private static class ColorButton extends JToggleButton {
+		private static final long serialVersionUID = 7759027446832437901L;
+		
+		private ColorButtonIcon icon;
 
+		public ColorButton(Color color) {
+			icon = new ColorButtonIcon(color);
+			setIcon(icon);
+		}
+		
+		public void setColor(Color color) {
+			icon.setColor(color);
+			repaint();
+		}
+
+		@Override public void setBounds(int x, int y, int width, int height) { icon.setMastersSize(  width,   height); super.setBounds(x, y, width, height); }
+		@Override public void setBounds(Rectangle r                        ) { icon.setMastersSize(r.width, r.height); super.setBounds(r                  ); }
+		@Override public void setSize  (Dimension d                        ) { icon.setMastersSize(d.width, d.height); super.setSize  (d                  ); }
+		@Override public void setSize  (              int width, int height) { icon.setMastersSize(  width,   height); super.setSize  (      width, height); }
+
+		private static class ColorButtonIcon implements Icon {
+
+			private static final Color BORDER_COLOR = new Color(128,128,128);
+			private int fakeWidth;
+			private int fakeHeight;
+			private int realWidth;
+			private int realHeight;
+			private Color color;
+
+			public ColorButtonIcon(Color color) {
+				this.color = color;
+				this.realWidth = 5;
+				this.realHeight = 5;
+				this.fakeWidth = 5;
+				this.fakeHeight = 5;
+			}
+
+			public void setColor(Color color) {
+				this.color = color;
+			}
+
+			public void setMastersSize(int masterWidth, int masterHeight) {
+				this.realWidth  = masterWidth -10;
+				this.realHeight = masterHeight-10;
+			}
+
+			@Override public int getIconWidth () { return fakeWidth; }
+			@Override public int getIconHeight() { return fakeHeight; }
+
+			@Override
+			public void paintIcon(Component c, Graphics g, int x, int y) {
+				int realX = x-(realWidth-fakeWidth)/2;
+				int realY = y-(realHeight-fakeHeight)/2;
+				if (color==null) {
+					g.setColor(BORDER_COLOR);
+					g.drawRect(realX, realY, realWidth, realHeight);
+				} else {
+					g.setColor(color);
+					g.fillRect(realX, realY, realWidth, realHeight);
+					g.setColor(BORDER_COLOR);
+					g.drawRect(realX, realY, realWidth, realHeight);
+				}
+			}
+			
+		}
+	}
 }
