90857159899aeaf422b256ca0f9dbc0d1a7b9f2e
Hendrik Scholtz <hendrik.github@nurfuerspam.de>
2016-02-26 08:46:37 +0000

    JAVA: [Eclipse] - Projekt "CommonLib"
    git-svn-id: file:///G/_svn_repo/programming/JAVA-Projects%20-%20Eclipse/CommonLib@2752 8592820e-bdd2-cc16-cb4b-ca06df7e8abd

--- src/net/schwarzbaer/gui/IconSource.java
+++ src/net/schwarzbaer/gui/IconSource.java
@@ -3,7 +3,6 @@ package net.schwarzbaer.gui;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.EnumMap;
 
 import javax.imageio.ImageIO;
 import javax.swing.Icon;
@@ -11,42 +10,33 @@ import javax.swing.ImageIcon;
 
 public abstract class IconSource<E extends Enum<E>> {
 	
-	private final EnumMap<E,Icon> icons;
 	private final int iconWidth;
 	private final int iconHeight;
+	private BufferedImage images;
 	
-	public IconSource(Class<E> keyType, int iconWidth, int iconHeight) {
+	public IconSource(int iconWidth, int iconHeight) {
 		this.iconWidth = iconWidth;
 		this.iconHeight = iconHeight;
-		this.icons = new EnumMap<E,Icon>(keyType);
+		this.images = null;
 	}
 	
-	public void readIconsFromResource(E[] availableKeys, String resourcePath) {
+	public void readIconsFromResource(String resourcePath) {
 //		String resourcePath = "/Toolbar.png";
 		InputStream stream = getClass().getResourceAsStream(resourcePath);
 		if (stream==null) {
 			System.err.printf("IconSource: Can't open resource stream \"%s\".\r\n",resourcePath);
 			return;
 		}
-		BufferedImage images;
 		try { images = ImageIO.read(stream); }
 		catch (IOException e) {
 			System.err.printf("IconSource: IOException while reading icon image file from resource \"%s\".",resourcePath);
 			return;
-		}
-		
-		icons.clear();
-		
-		for (E key:availableKeys) {
-			int index = getIconIndexInImage(key);
-			BufferedImage subimage = images.getSubimage( index*iconWidth,0, iconWidth,iconHeight );
-			icons.put(key, new ImageIcon(subimage));
 		}
 	}
 	
 	protected abstract int getIconIndexInImage(E key);
 	
 	public Icon getIcon(E key) {
-		return icons.get(key);
+		return new ImageIcon(images.getSubimage( getIconIndexInImage(key)*iconWidth,0, iconWidth,iconHeight ));
 	}
 }
