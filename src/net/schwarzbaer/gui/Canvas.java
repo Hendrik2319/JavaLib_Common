327c9a90cad800fcec6809915f228726fe705b4e
Hendrik Scholtz <hendrik.github@nurfuerspam.de>
2017-12-12 22:47:24 +0000

    JAVA: [Eclipse] - Projekt "CommonLib_gui_essentials"
    git-svn-id: file:///G/_svn_repo/programming/JAVA-Projects%20-%20Eclipse/CommonLib@3188 8592820e-bdd2-cc16-cb4b-ca06df7e8abd

--- src/net/schwarzbaer/gui/Canvas.java
+++ src/net/schwarzbaer/gui/Canvas.java
@@ -12,6 +12,7 @@ import javax.swing.JComponent;
 public abstract class Canvas extends JComponent {
 	private static final long serialVersionUID = 1936784818314303929L;
 	
+	protected boolean withDebugOutput;
 	protected int width;
     protected int height;
     protected int preferredWidth;
@@ -24,6 +25,7 @@ public abstract class Canvas extends JComponent {
         this.height = -1;
         this.preferredWidth  = preferredWidth;
         this.preferredHeight = preferredHeight;
+        this.withDebugOutput = false;
     }
     protected void sizeChanged( int width, int height ) {}
     protected abstract void paintCanvas(Graphics g, int width, int height );
@@ -33,21 +35,46 @@ public abstract class Canvas extends JComponent {
         paintCanvas( g, width, height );
     }
     
-    
     public void setPreferredSize( int preferredWidth, int preferredHeight ) {
+    	if (withDebugOutput) System.out.printf("setPreferredSize( %d, %d )\r\n", preferredWidth,preferredHeight);
         this.preferredWidth  = preferredWidth;
         this.preferredHeight = preferredHeight;
     }
     @Override
 	public void setPreferredSize(Dimension prefSize) {
+    	if (withDebugOutput) System.out.printf("setPreferredSize( %s )\r\n", prefSize);
 		super.setPreferredSize(prefSize);
         this.preferredWidth  = prefSize.width;
         this.preferredHeight = prefSize.height;
 	}
 
-	@Override public Dimension getPreferredSize() { return new Dimension( preferredWidth, preferredHeight ); }
-    @Override public void setBounds(int x, int y, int width, int height) { super.setBounds( x, y, width, height ); this.width = width; this.height = height; sizeChanged( width, height ); }
-    @Override public void setBounds(Rectangle r)         { super.setBounds( r );           this.width = r.width; this.height = r.height; sizeChanged( width, height ); }
-    @Override public void setSize(Dimension d)           { super.setSize( d );             this.width = d.width; this.height = d.height; sizeChanged( width, height ); }
-    @Override public void setSize(int width, int height) { super.setSize( width, height ); this.width =   width; this.height =   height; sizeChanged( width, height ); }
+	@Override public Dimension getPreferredSize() {
+    	if (withDebugOutput) System.out.printf("getPreferredSize() -> ( %d, %d )\r\n", preferredWidth,preferredHeight);
+		return new Dimension( preferredWidth, preferredHeight );
+	}
+	
+    @Override public void setBounds(int x, int y, int width, int height) {
+    	if (withDebugOutput) System.out.printf("setBounds( %d, %d, %d, %d )\r\n", x, y, width, height);
+    	super.setBounds( x, y, width, height );
+    	this.width = width; this.height = height;
+    	sizeChanged( width, height );
+    }
+    @Override public void setBounds(Rectangle r) {
+    	if (withDebugOutput) System.out.printf("setBounds( %s )\r\n", r);
+    	super.setBounds( r );
+    	this.width = r.width; this.height = r.height;
+    	sizeChanged( width, height );
+    }
+    @Override public void setSize(Dimension d) {
+    	if (withDebugOutput) System.out.printf("setSize( %s )\r\n", d);
+    	super.setSize( d );
+    	this.width = d.width; this.height = d.height;
+    	sizeChanged( width, height );
+    }
+    @Override public void setSize(int width, int height) {
+    	if (withDebugOutput) System.out.printf("setSize( %d, %d )\r\n", width, height);
+    	super.setSize( width, height );
+    	this.width =   width; this.height =   height;
+    	sizeChanged( width, height );
+    }
 }
