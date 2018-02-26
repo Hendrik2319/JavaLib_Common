4e02d3cf9dd2883011e2e8a1a3e88b458a3f270d
Hendrik Scholtz <hendrik.github@nurfuerspam.de>
2018-02-26 19:09:19 +0000

    JAVA: [Eclipse] - Projekt "CommonLib_gui_essentials"
    git-svn-id: file:///G/_svn_repo/programming/JAVA-Projects%20-%20Eclipse/CommonLib@3300 8592820e-bdd2-cc16-cb4b-ca06df7e8abd

--- src/net/schwarzbaer/gui/Tables.java
+++ src/net/schwarzbaer/gui/Tables.java
@@ -1,5 +1,158 @@
 package net.schwarzbaer.gui;
 
+import java.util.Vector;
+
+import javax.swing.JTable;
+import javax.swing.event.TableModelEvent;
+import javax.swing.event.TableModelListener;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
+import javax.swing.table.TableModel;
+
 public class Tables {
+
+	public static class SimplifiedColumnConfig {
+		public String name;
+		public int minWidth;
+		public int maxWidth;
+		public int prefWidth;
+		public int currentWidth;
+		public Class<?> columnClass;
+		
+		SimplifiedColumnConfig() {
+			this("",String.class,-1,-1,-1,-1);
+		}
+		public SimplifiedColumnConfig(String name, Class<?> columnClass, int minWidth, int maxWidth, int prefWidth, int currentWidth) {
+			this.name = name;
+			this.columnClass = columnClass;
+			this.minWidth = minWidth;
+			this.maxWidth = maxWidth;
+			this.prefWidth = prefWidth;
+			this.currentWidth = currentWidth;
+		}
+	}
+
+	public static interface SimplifiedColumnIDInterface {
+		public SimplifiedColumnConfig getColumnConfig();
+	}
+
+	public static abstract class SimplifiedTableModel<ColumnID extends Enum<ColumnID> & SimplifiedColumnIDInterface> implements TableModel {
+		
+		protected ColumnID[] columns;
+		private Vector<TableModelListener> tableModelListeners;
+	
+		protected SimplifiedTableModel(ColumnID[] columns) {
+			this.columns = columns;
+			tableModelListeners = new Vector<>();
+		}
+	
+		@Override public void addTableModelListener(TableModelListener l) { tableModelListeners.add(l); }
+		@Override public void removeTableModelListener(TableModelListener l) { tableModelListeners.remove(l); }
+		
+		protected void fireTableModelEvent(TableModelEvent e) {
+			for (TableModelListener tml:tableModelListeners)
+				tml.tableChanged(e);
+		}
+		protected void fireTableColumnUpdate(int columnIndex) {
+			if (getRowCount()>0)
+				fireTableModelEvent(new TableModelEvent(this, 0, getRowCount()-1, columnIndex, TableModelEvent.UPDATE));
+		}
+		protected void fireTableCellUpdate(int rowIndex, int columnIndex) {
+			fireTableModelEvent(new TableModelEvent(this, rowIndex, rowIndex, columnIndex, TableModelEvent.UPDATE));
+		}
+		protected void fireTableRowAdded(int rowIndex) {
+			fireTableModelEvent(new TableModelEvent(this, rowIndex, rowIndex, TableModelEvent.ALL_COLUMNS, TableModelEvent.INSERT));
+		}
+		protected void fireTableUpdate() {
+			fireTableModelEvent(new TableModelEvent(this));
+		}
+		protected void fireTableStructureUpdate() {
+			fireTableModelEvent(new TableModelEvent(this,TableModelEvent.HEADER_ROW));
+		}
+		
+		public void initiateColumnUpdate(ColumnID columnID) {
+			int columnIndex = getColumn( columnID );
+			if (columnIndex>=0) fireTableColumnUpdate(columnIndex);
+		}
+
+		@Override public abstract int getRowCount();
+		public abstract Object getValueAt(int rowIndex, int columnIndex, ColumnID columnID);
+		
+		public int getUnsortedRowsCount() { return 0; }
+		
+		protected ColumnID getColumnID(int columnIndex) {
+			if (columnIndex<0) return null;
+			if (columnIndex<columns.length) return columns[columnIndex];
+			return null;
+		}
+		public int getColumn( ColumnID columnID ) {
+			for (int i=0; i<columns.length; ++i)
+				if (columns[i]==columnID)
+					return i;
+			return -1;
+		}
+		
+		@Override public int getColumnCount() { return columns.length; }
+		
+		@Override
+		public String getColumnName(int columnIndex) {
+			ColumnID columnID = getColumnID(columnIndex);
+			if (columnID==null) return null;
+			return columnID.getColumnConfig().name; //getName();
+		}
+	
+		@Override
+		public Class<?> getColumnClass(int columnIndex) {
+			ColumnID columnID = getColumnID(columnIndex);
+			if (columnID==null) return null;
+			return columnID.getColumnConfig().columnClass; //getColumnClass();
+		}
+	
+		@Override
+		public Object getValueAt(int rowIndex, int columnIndex) {
+			if (rowIndex<0) return null;
+			if (rowIndex>=getRowCount()) return null;
+			ColumnID columnID = getColumnID(columnIndex);
+			if (columnID==null) return null;
+			return getValueAt(rowIndex, columnIndex, columnID);
+		}
+	
+		@Override public boolean isCellEditable(int rowIndex, int columnIndex) {
+			if (rowIndex<0) return false;
+			if (rowIndex>=getRowCount()) return false;
+			ColumnID columnID = getColumnID(columnIndex);
+			if (columnID==null) return false;
+			return isCellEditable(rowIndex, columnIndex, columnID);
+		}
+		protected boolean isCellEditable(int rowIndex, int columnIndex, ColumnID columnID) { return false; }
+	
+		@Override public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+			if (rowIndex<0) return;
+			if (rowIndex>=getRowCount()) return;
+			ColumnID columnID = getColumnID(columnIndex);
+			if (columnID==null) return;
+			setValueAt(aValue, rowIndex, columnIndex, columnID);
+		}
+		protected void setValueAt(Object aValue, int rowIndex, int columnIndex, ColumnID columnID) {}
+	
+		public void setColumnWidths(JTable table) {
+			TableColumnModel columnModel = table.getColumnModel();
+			for (int i=0; i<columnModel.getColumnCount(); ++i) {
+				ColumnID columnID = getColumnID(i);
+				if (columnID!=null) {
+					SimplifiedColumnConfig config = columnID.getColumnConfig();
+					setColumnWidth(columnModel.getColumn(i), config.minWidth, config.maxWidth, config.prefWidth, config.currentWidth);
+				}
+			}
+		}
+	
+		private void setColumnWidth(TableColumn column, int min, int max, int preferred, int width) {
+			if (min>=0) column.setMinWidth(min);
+			if (max>=0) column.setMinWidth(max);
+			if (preferred>=0) column.setPreferredWidth(preferred);
+			if (width    >=0) column.setWidth(width);
+		}
+	}
+
 
 }
