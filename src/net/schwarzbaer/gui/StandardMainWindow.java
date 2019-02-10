9eedaecb63d408bb5613b0eb64e2324449911077
Hendrik Scholtz <hendrik.github@nurfuerspam.de>
2019-02-10 08:51:52 +0000

    JAVA: [Eclipse] - Projekt "CommonLib"
    git-svn-id: file:///G/_svn_repo/programming/JAVA-Projects%20-%20Eclipse/CommonLib@3684 8592820e-bdd2-cc16-cb4b-ca06df7e8abd

--- src/net/schwarzbaer/gui/StandardMainWindow.java
+++ src/net/schwarzbaer/gui/StandardMainWindow.java
@@ -1,8 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package net.schwarzbaer.gui;
 
 import java.awt.Dimension;
@@ -15,10 +10,6 @@ import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
 
-/**
- *
- * @author Hendrik
- */
 public class StandardMainWindow extends JFrame implements WindowListener {
 	private static final long serialVersionUID = -6515512014217265169L;
 	
