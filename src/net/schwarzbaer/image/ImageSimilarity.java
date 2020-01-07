3b4253662782ee7aedae5cd406f55d2838a83ef0
Hendrik Scholtz <hendrik.github@nurfuerspam.de>
2020-01-07 16:02:15 +0000

    JAVA: [Eclipse] - Projekt "CommonLib"
    git-svn-id: file:///G/_svn_repo/programming/JAVA-Projects%20-%20Eclipse/CommonLib@4276 8592820e-bdd2-cc16-cb4b-ca06df7e8abd

---
+++ src/net/schwarzbaer/image/ImageSimilarity.java
@@ -0,0 +1,65 @@
+package net.schwarzbaer.image;
+
+import java.awt.Rectangle;
+import java.awt.image.WritableRaster;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class ImageSimilarity<ImageID> {
+	
+	private RasterSource<ImageID> rasterSource;
+	
+	ImageSimilarity(RasterSource<ImageID> rasterSource) {
+		this.rasterSource = rasterSource;
+	}
+	
+	public int[] computeOrder(ImageID baseImage_, ImageID[] images_) {
+		ComparableImage baseImage = new ComparableImage(-1,rasterSource.createRaster(baseImage_,0xFFFFFF,256,256));
+		
+		ComparableImage[] images = new ComparableImage[images_.length];
+		for (int i=0; i<images_.length; i++) {
+			images[i] = new ComparableImage(i,rasterSource.createRaster(images_[i],0xFFFFFF,256,256));
+			images[i].similarity = images[i].computeSimilarityTo(baseImage);
+		}
+		
+		int[] sortedIndexes = Arrays.stream(images)
+				.sorted(Comparator.comparing(img->img.similarity))
+				.mapToInt(img->img.index)
+				.toArray();
+		return sortedIndexes;
+	}
+	
+	private static class ComparableImage {
+		final WritableRaster raster;
+		final int index;
+		double similarity;
+
+		private ComparableImage(int index, WritableRaster raster) {
+			this.raster = raster;
+			this.index = index;
+			this.similarity = Double.NaN;
+		}
+
+		private double computeSimilarityTo(ComparableImage other) {
+			Rectangle bounds      = this .raster.getBounds();
+			Rectangle otherBounds = other.raster.getBounds();
+			if (!bounds.equals(otherBounds))
+				throw new IllegalArgumentException();
+			
+			double[] p1 = new double[4];
+			double[] p2 = new double[4];
+			double similarity = 0;
+			for (int x=bounds.x; x<bounds.width+bounds.x; x++)
+				for (int y=bounds.y; y<bounds.height+bounds.y; y++) {
+					this .raster.getPixel(x, y, p1);
+					other.raster.getPixel(x, y, p2);
+					similarity += (float) Math.sqrt( (p1[0]-p2[0])*(p1[0]-p2[0]) + (p1[1]-p2[1])*(p1[1]-p2[1]) + (p1[2]-p2[2])*(p1[2]-p2[2]) );
+				}
+			return similarity;
+		}
+	}
+	
+	public interface RasterSource<ImageID> {
+		WritableRaster createRaster(ImageID image, int backgroundColor, int width, int height);
+	}
+}
