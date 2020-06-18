1ac043b2f2c7389f23c0e2ca55992f61b04a5010
Hendrik Scholtz <hendrik.github@nurfuerspam.de>
2020-06-18 17:38:53 +0000

    JAVA: [Eclipse] - Projekt "CommonLib"
    git-svn-id: file:///G/_svn_repo/programming/JAVA-Projects%20-%20Eclipse/CommonLib@4705 8592820e-bdd2-cc16-cb4b-ca06df7e8abd

--- src/net/schwarzbaer/image/bumpmapping/NormalFunction.java
+++ src/net/schwarzbaer/image/bumpmapping/NormalFunction.java
@@ -3,6 +3,7 @@ package net.schwarzbaer.image.bumpmapping;
 import java.awt.Color;
 
 import net.schwarzbaer.image.bumpmapping.BumpMapping.Colorizer;
+import net.schwarzbaer.image.bumpmapping.BumpMapping.MutableNormal;
 import net.schwarzbaer.image.bumpmapping.BumpMapping.Normal;
 import net.schwarzbaer.image.bumpmapping.BumpMapping.NormalXY;
 
@@ -25,6 +26,80 @@ public interface NormalFunction {
 		@Override public void forceNormalCreation(boolean force) {}
 		
 	}
+	
+	public static class NormalMap implements NormalFunction {
+		
+		private Normal[][] normalMap;
+		private boolean forceNormalCreation;
+		private boolean centered;
+		
+		public NormalMap(Normal[][] normalMap, boolean centered) {
+			this.normalMap = normalMap;
+			this.centered = centered;
+			forceNormalCreation = true;
+		}
+		@Override
+		public Normal getNormal(double x, double y, double width, double height) {
+			int mapWidth  = normalMap    .length;
+			int xi = (int) Math.round(x + (centered ? (mapWidth -width )/2 : 0));
+			if (xi<0 || xi>=mapWidth ) return forceNormalCreation ? new Normal(0,0,1) : null;
+			
+			int mapHeight = normalMap[xi].length;
+			int yi = (int) Math.round(y + (centered ? (mapHeight-height)/2 : 0));
+			if (yi<0 || yi>=mapHeight) return forceNormalCreation ? new Normal(0,0,1) : null;
+			
+			return normalMap[xi][yi];
+		}
+		@Override public void forceNormalCreation(boolean forceNormalCreation) {
+			this.forceNormalCreation = forceNormalCreation;
+		}
+		
+		public static NormalMap createFromHeightMap(float[][] heightMap, double cornerScale) {
+			return createFromHeightMap(heightMap,null,cornerScale);
+		}
+		public static NormalMap createFromHeightMap(float[][] heightMap, Color[][] colorMap, double cornerScale) {
+			int width = heightMap.length;
+			int height = heightMap[0].length;;
+			Normal[][] normalMap = new Normal[width][height];
+			for (int x1=0; x1<width; ++x1)
+				for (int y1=0; y1<height; ++y1) {
+					MutableNormal base = new MutableNormal(0,0,0,colorMap==null?null:colorMap[x1][y1]);
+					addNormal(base,computeNormal(heightMap,x1,y1,+1, 0),1); 
+					addNormal(base,computeNormal(heightMap,x1,y1, 0,+1),1); 
+					addNormal(base,computeNormal(heightMap,x1,y1,-1, 0),1); 
+					addNormal(base,computeNormal(heightMap,x1,y1, 0,-1),1);
+					if (cornerScale>0) {
+						addNormal(base,computeNormal(heightMap,x1,y1,+1,-1),cornerScale);
+						addNormal(base,computeNormal(heightMap,x1,y1,+1,+1),cornerScale); 
+						addNormal(base,computeNormal(heightMap,x1,y1,-1,+1),cornerScale); 
+						addNormal(base,computeNormal(heightMap,x1,y1,-1,-1),cornerScale); 
+					}
+					normalMap[x1][y1] = base.toNormal().normalize();
+				}
+			return new NormalMap(normalMap, false);
+		}
+		private static void addNormal(MutableNormal base, Normal n, double scale) {
+			if (n != null) {
+				base.x += n.x*scale;
+				base.y += n.y*scale;
+				base.z += n.z*scale;
+			}
+		}
+		private static Normal computeNormal(float[][] heightMap, int x, int y, int dx, int dy) {
+			if (x+dx<0 || x+dx>=heightMap   .length) return null;
+			if (y+dy<0 || y+dy>=heightMap[0].length) return null;
+			float dh = heightMap[x][y]-heightMap[x+dx][y+dy];
+			if ( (dx!=0) && (dy!=0) ) {
+				double w = Math.atan2(dy, dx);
+				double r = Math.sqrt(dx*dx+dy*dy);
+				return new Normal(dh,0,r).normalize().rotateZ(w);
+			}
+			if (dx!=0) return new Normal(dh*dx,0,Math.abs(dx)).normalize();
+			if (dy!=0) return new Normal(0,dh*dy,Math.abs(dy)).normalize();
+			return null;
+		}
+	}
+	
 	public static interface Cart extends NormalFunction {
 		@Override public default Normal  getNormal     (double x, double y, double width, double height) { return getNormal     (x, y); }
 		public Normal getNormal(double x, double y);
@@ -54,14 +129,14 @@ public interface NormalFunction {
 			@Override public void forceNormalCreation(boolean force) {}
 			
 		}
-		public static abstract class AbstractNormalFunctionPolar<MyClass extends AbstractNormalFunctionPolar<MyClass>> implements Polar {
+		public static abstract class AbstractPolar<MyClass extends AbstractPolar<MyClass>> implements Polar {
 			
 			private Colorizer.Polar colorizer;
 			private ExtraNormalFunction.Polar extras;
 			private boolean forceNormalCreation;
 			private boolean showExtrasOnly;
 		
-			public AbstractNormalFunctionPolar() {
+			public AbstractPolar() {
 				this.colorizer = null;
 				extras = null;
 				forceNormalCreation = false;
@@ -117,7 +192,7 @@ public interface NormalFunction {
 			
 			protected abstract Normal getBaseNormal(double w, double r);
 		}
-		public static class RotatedProfile extends AbstractNormalFunctionPolar<RotatedProfile> {
+		public static class RotatedProfile extends AbstractPolar<RotatedProfile> {
 			
 			private ProfileXY profile;
 		
