04f85ad9b4a2723311f7d32b9e42a2e9202e5fbb
Hendrik Scholtz <hendrik.github@nurfuerspam.de>
2020-06-20 12:55:27 +0000

    JAVA: [Eclipse] - Projekt "CommonLib"
    git-svn-id: file:///G/_svn_repo/programming/JAVA-Projects%20-%20Eclipse/CommonLib@4726 8592820e-bdd2-cc16-cb4b-ca06df7e8abd

--- src/net/schwarzbaer/system/Settings.java
+++ src/net/schwarzbaer/system/Settings.java
@@ -6,7 +6,7 @@ import java.util.prefs.Preferences;
 
 public class Settings<ValueGroup extends Enum<ValueGroup> & Settings.GroupKeys<ValueKey>, ValueKey extends Enum<ValueKey>> {
 
-	interface GroupKeys<V> {
+	public interface GroupKeys<V> {
 		V[] getKeys();
 	}
 	
@@ -20,25 +20,25 @@ public class Settings<ValueGroup extends Enum<ValueGroup> & Settings.GroupKeys<V
 		return contains(valueGroup.getKeys());
 	}
 	
-	protected boolean getBool  (ValueKey key               ) { return preferences.getBoolean(key.toString(), true ); }
-	protected boolean getBool  (ValueKey key, boolean def  ) { return preferences.getBoolean(key.toString(), def  ); }
-	protected void    putBool  (ValueKey key, boolean value) {        preferences.putBoolean(key.toString(), value); }
-	protected float   getFloat (ValueKey key               ) { return preferences.getFloat  (key.toString(), 0    ); }
-	protected float   getFloat (ValueKey key, float def    ) { return preferences.getFloat  (key.toString(), def  ); }
-	protected void    putFloat (ValueKey key, float value  ) {        preferences.putFloat  (key.toString(), value); }
-	protected double  getDouble(ValueKey key               ) { return preferences.getDouble (key.toString(), 0    ); }
-	protected double  getDouble(ValueKey key, double def   ) { return preferences.getDouble (key.toString(), def  ); }
-	protected void    putDouble(ValueKey key, double value ) {        preferences.putDouble (key.toString(), value); }
-	protected int     getInt   (ValueKey key               ) { return preferences.getInt    (key.toString(), 0    ); }
-	protected int     getInt   (ValueKey key, int def      ) { return preferences.getInt    (key.toString(), def  ); }
-	protected void    putInt   (ValueKey key, int value    ) {        preferences.putInt    (key.toString(), value); }
-	protected String  getString(ValueKey key               ) { return preferences.get       (key.toString(), null ); }
-	protected String  getString(ValueKey key, String def   ) { return preferences.get       (key.toString(), def  ); }
-	protected void    putString(ValueKey key, String value ) {        preferences.put       (key.toString(), value); }
+	public boolean getBool  (ValueKey key               ) { return preferences.getBoolean(key.toString(), true ); }
+	public boolean getBool  (ValueKey key, boolean def  ) { return preferences.getBoolean(key.toString(), def  ); }
+	public void    putBool  (ValueKey key, boolean value) {        preferences.putBoolean(key.toString(), value); }
+	public float   getFloat (ValueKey key               ) { return preferences.getFloat  (key.toString(), 0    ); }
+	public float   getFloat (ValueKey key, float def    ) { return preferences.getFloat  (key.toString(), def  ); }
+	public void    putFloat (ValueKey key, float value  ) {        preferences.putFloat  (key.toString(), value); }
+	public double  getDouble(ValueKey key               ) { return preferences.getDouble (key.toString(), 0    ); }
+	public double  getDouble(ValueKey key, double def   ) { return preferences.getDouble (key.toString(), def  ); }
+	public void    putDouble(ValueKey key, double value ) {        preferences.putDouble (key.toString(), value); }
+	public int     getInt   (ValueKey key               ) { return preferences.getInt    (key.toString(), 0    ); }
+	public int     getInt   (ValueKey key, int def      ) { return preferences.getInt    (key.toString(), def  ); }
+	public void    putInt   (ValueKey key, int value    ) {        preferences.putInt    (key.toString(), value); }
+	public String  getString(ValueKey key               ) { return preferences.get       (key.toString(), null ); }
+	public String  getString(ValueKey key, String def   ) { return preferences.get       (key.toString(), def  ); }
+	public void    putString(ValueKey key, String value ) {        preferences.put       (key.toString(), value); }
 
-	protected Color   getColor (ValueKey key              ) { return new Color(getInt(key, Color.BLACK.getRGB()), true); }
-	protected Color   getColor (ValueKey key, Color def   ) { return new Color(getInt(key,         def.getRGB()), true); }
-	protected void    putColor (ValueKey key, Color value ) { putInt(key, value.getRGB()); }
+	public Color   getColor (ValueKey key              ) { return new Color(getInt(key, Color.BLACK.getRGB()), true); }
+	public Color   getColor (ValueKey key, Color def   ) { return new Color(getInt(key,         def.getRGB()), true); }
+	public void    putColor (ValueKey key, Color value ) { putInt(key, value.getRGB()); }
 
 
 	private boolean contains(String[] prefkeys, ValueKey key) {
@@ -50,11 +50,11 @@ public class Settings<ValueGroup extends Enum<ValueGroup> & Settings.GroupKeys<V
 		return false;
 	}
 
-	protected boolean contains(ValueKey key1                                             ) { return contains(key1, null, null, null, null); }
-	protected boolean contains(ValueKey key1, ValueKey key2                              ) { return contains(key1, key2, null, null, null); }
-	protected boolean contains(ValueKey key1, ValueKey key2, ValueKey key3               ) { return contains(key1, key2, key3, null, null); }
-	protected boolean contains(ValueKey key1, ValueKey key2, ValueKey key3, ValueKey key4) { return contains(key1, key2, key3, key4, null); }
-	protected boolean contains(ValueKey key1, ValueKey key2, ValueKey key3, ValueKey key4, ValueKey key5) {
+	public boolean contains(ValueKey key1                                             ) { return contains(key1, null, null, null, null); }
+	public boolean contains(ValueKey key1, ValueKey key2                              ) { return contains(key1, key2, null, null, null); }
+	public boolean contains(ValueKey key1, ValueKey key2, ValueKey key3               ) { return contains(key1, key2, key3, null, null); }
+	public boolean contains(ValueKey key1, ValueKey key2, ValueKey key3, ValueKey key4) { return contains(key1, key2, key3, key4, null); }
+	public boolean contains(ValueKey key1, ValueKey key2, ValueKey key3, ValueKey key4, ValueKey key5) {
 		String[] prefkeys;
 		try { prefkeys = preferences.keys(); }
 		catch (BackingStoreException e) { e.printStackTrace(); return false; }
@@ -66,7 +66,7 @@ public class Settings<ValueGroup extends Enum<ValueGroup> & Settings.GroupKeys<V
 		return true;
 	}
 
-	protected boolean contains(ValueKey[] keys) {
+	public boolean contains(ValueKey[] keys) {
 		String[] prefkeys;
 		try { prefkeys = preferences.keys(); }
 		catch (BackingStoreException e) { e.printStackTrace(); return false; }
