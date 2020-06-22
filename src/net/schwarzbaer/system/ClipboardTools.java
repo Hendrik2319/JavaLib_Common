16fea3e87cdf18c5e65b42b726eade8da4036d89
Hendrik Scholtz <hendrik.github@nurfuerspam.de>
2020-06-22 10:48:09 +0000

    JAVA: [Eclipse] - Projekt "CommonLib"
    git-svn-id: file:///G/_svn_repo/programming/JAVA-Projects%20-%20Eclipse/CommonLib@4745 8592820e-bdd2-cc16-cb4b-ca06df7e8abd

--- src/net/schwarzbaer/system/ClipboardTools.java
+++ src/net/schwarzbaer/system/ClipboardTools.java
@@ -1,9 +1,11 @@
 package net.schwarzbaer.system;
 
 import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.Transferable;
 import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.Reader;
 import java.io.StringWriter;
@@ -12,17 +14,39 @@ import javax.activation.DataHandler;
 
 public class ClipboardTools {
 
+	public static void copyToClipBoard(BufferedImage image) {
+		copyToClipBoard(new TransferableImage(image));
+	}
+
 	public static void copyToClipBoard(String str) {
+		copyToClipBoard(new DataHandler(str,"text/plain"));
+	}
+
+	public static void copyToClipBoard(Transferable content) {
 		Toolkit toolkit = Toolkit.getDefaultToolkit();
-		java.awt.datatransfer.Clipboard clipboard = toolkit.getSystemClipboard();
-		DataHandler content = new DataHandler(str,"text/plain");
+		Clipboard clipboard = toolkit.getSystemClipboard();
 		try { clipboard.setContents(content,null); }
 		catch (IllegalStateException e1) { e1.printStackTrace(); }
 	}
 
-	public static String getStringFromClipBoard() {
+	private static class TransferableImage implements Transferable {
+
+		private BufferedImage image;
+		public TransferableImage(BufferedImage image) { this.image = image; }
+
+		@Override public Object getTransferData(DataFlavor flavor) throws UnsupportedFlavorException, IOException {
+			if (isDataFlavorSupported(flavor)) return image;
+			throw new UnsupportedFlavorException( flavor );
+		}
+
+		@Override public DataFlavor[] getTransferDataFlavors() { return new DataFlavor[] { DataFlavor.imageFlavor }; }
+		@Override public boolean isDataFlavorSupported(DataFlavor flavor) { return DataFlavor.imageFlavor.equals(flavor); }
+		
+	}
+
+	public static String getStringFromClipBoard(boolean showOtherDataFlavors) {
 		Toolkit toolkit = Toolkit.getDefaultToolkit();
-		java.awt.datatransfer.Clipboard clipboard = toolkit.getSystemClipboard();
+		Clipboard clipboard = toolkit.getSystemClipboard();
 		Transferable transferable = clipboard.getContents(null);
 		if (transferable==null) return null;
 		
@@ -32,7 +56,7 @@ public class ClipboardTools {
 			DataFlavor[] transferDataFlavors = transferable.getTransferDataFlavors();
 			if (transferDataFlavors==null || transferDataFlavors.length==0) return null;
 			
-			System.out.println("transferDataFlavors: "+toString(transferDataFlavors));
+			if (showOtherDataFlavors) System.out.println("transferDataFlavors: "+toString(transferDataFlavors));
 			textFlavor = DataFlavor.selectBestTextFlavor(transferDataFlavors);
 		}
 		
