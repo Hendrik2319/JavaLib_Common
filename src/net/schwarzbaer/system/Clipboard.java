9024fec4bde2f6ca55e825325bcfb4d494e45688
Hendrik Scholtz <hendrik.github@nurfuerspam.de>
2020-06-22 10:30:28 +0000

    JAVA: [Eclipse] - Projekt "CommonLib"
    git-svn-id: file:///G/_svn_repo/programming/JAVA-Projects%20-%20Eclipse/CommonLib@4739 8592820e-bdd2-cc16-cb4b-ca06df7e8abd

--- src/net/schwarzbaer/system/Clipboard.java
+++ src/net/schwarzbaer/system/Clipboard.java
@@ -1,5 +1,63 @@
 package net.schwarzbaer.system;
 
+import java.awt.Toolkit;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringWriter;
+
+import javax.activation.DataHandler;
+
 public class Clipboard {
 
+	public static void copyToClipBoard(String str) {
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
+		java.awt.datatransfer.Clipboard clipboard = toolkit.getSystemClipboard();
+		DataHandler content = new DataHandler(str,"text/plain");
+		try { clipboard.setContents(content,null); }
+		catch (IllegalStateException e1) { e1.printStackTrace(); }
+	}
+
+	public static String getStringFromClipBoard() {
+		Toolkit toolkit = Toolkit.getDefaultToolkit();
+		java.awt.datatransfer.Clipboard clipboard = toolkit.getSystemClipboard();
+		Transferable transferable = clipboard.getContents(null);
+		if (transferable==null) return null;
+		
+		DataFlavor textFlavor = new DataFlavor(String.class, "text/plain; class=<java.lang.String>");
+		
+		if (!transferable.isDataFlavorSupported(textFlavor)) {
+			DataFlavor[] transferDataFlavors = transferable.getTransferDataFlavors();
+			if (transferDataFlavors==null || transferDataFlavors.length==0) return null;
+			
+			System.out.println("transferDataFlavors: "+toString(transferDataFlavors));
+			textFlavor = DataFlavor.selectBestTextFlavor(transferDataFlavors);
+		}
+		
+		if (textFlavor==null) return null;
+		
+		Reader reader;
+		try { reader = textFlavor.getReaderForText(transferable); }
+		catch (UnsupportedFlavorException | IOException e) { return null; }
+		StringWriter sw = new StringWriter();
+		
+		int n; char[] cbuf = new char[100000];
+		try { while ((n=reader.read(cbuf))>=0) if (n>0) sw.write(cbuf, 0, n); }
+		catch (IOException e) {}
+		
+		try { reader.close(); } catch (IOException e) {}
+		return sw.toString();
+	}
+
+	private static String toString(DataFlavor[] dataFlavors) {
+		if (dataFlavors==null) return "<null>";
+		String str = "";
+		for (DataFlavor df:dataFlavors) {
+			if (!str.isEmpty()) str+=",\r\n";
+			str+=""+df;
+		}
+		return "[\r\n"+str+"\r\n]";
+	}
 }
