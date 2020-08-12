15b689ddc6cc7ee9f7796d507b73afcc9353a58e
Hendrik Scholtz <hendrik.github@nurfuerspam.de>
2020-08-12 18:50:06 +0000

    JAVA: [Eclipse] - Projekt "CommonLib"
    git-svn-id: file:///G/_svn_repo/programming/JAVA-Projects%20-%20Eclipse/CommonLib@4894 8592820e-bdd2-cc16-cb4b-ca06df7e8abd

--- src/net/schwarzbaer/gui/ImageViewDialog.java
+++ src/net/schwarzbaer/gui/ImageViewDialog.java
@@ -7,13 +7,19 @@ import javax.swing.JFrame;
 
 public class ImageViewDialog extends JDialog {
 	private static final long serialVersionUID = 2981906616002170627L;
+	private ImageView imageView;
 
 	public ImageViewDialog(JFrame parent, BufferedImage image, String title, int width, int height) {
 		super(parent,title,ModalityType.APPLICATION_MODAL);
-		ImageView imageView = new ImageView(image,width,height);
+		imageView = new ImageView(image,width,height);
 		setContentPane(imageView);
 		pack();
 		setLocationRelativeTo(parent);
+		imageView.reset();
+	}
+
+	public void setImage(BufferedImage image) {
+		imageView.setImage(image);
 		imageView.reset();
 	}
 }
