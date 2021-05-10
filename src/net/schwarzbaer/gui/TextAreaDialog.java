54bd896496c64c2809e9c03fc1c4995b7244a630
Hendrik Scholtz <hendrik.github@nurfuerspam.de>
2021-05-10 12:57:26 +0000

    JAVA: [Eclipse] - Projekt "CommonLib"
    git-svn-id: file:///G/_svn_repo/programming/JAVA-Projects%20-%20Eclipse/CommonLib@5656 8592820e-bdd2-cc16-cb4b-ca06df7e8abd

--- src/net/schwarzbaer/gui/TextAreaDialog.java
+++ src/net/schwarzbaer/gui/TextAreaDialog.java
@@ -3,8 +3,13 @@ package net.schwarzbaer.gui;
 import java.awt.Dimension;
 import java.awt.Window;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.function.Consumer;
 
 import javax.swing.JButton;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
@@ -13,11 +18,25 @@ public class TextAreaDialog extends StandardDialog {
 	
 	private final JTextArea textArea;
 
-	private TextAreaDialog(Window parent, String title, ModalityType modality, boolean repeatedUseOfDialogObject, int width, int height, boolean textEditable) {
+	private TextAreaDialog(Window parent, String title, ModalityType modality, boolean repeatedUseOfDialogObject, int width, int height, boolean textEditable, boolean textAreaLineWrap) {
 		super(parent, title, modality, repeatedUseOfDialogObject);
 		
+		JPopupMenu textViewContextMenu = new JPopupMenu();
 		textArea = new JTextArea();
 		textArea.setEditable(textEditable);
+		textArea.addMouseListener(new MouseAdapter() {
+			@Override public void mouseClicked(MouseEvent e) {
+				if (e.getButton()==MouseEvent.BUTTON3)
+					textViewContextMenu.show(textArea, e.getX(), e.getY());
+			}
+		});
+		
+		textArea.setLineWrap(textAreaLineWrap);
+		textArea.setWrapStyleWord(textAreaLineWrap);
+		textViewContextMenu.add(createCheckBoxMenuItem("Line Wrap", textAreaLineWrap, isChecked->{
+			textArea.setLineWrap(isChecked);
+			textArea.setWrapStyleWord(isChecked);
+		}) );
 		
 		JScrollPane textAreaScrollPane = new JScrollPane(textArea);
 		textAreaScrollPane.setPreferredSize(new Dimension(width,height));
@@ -25,8 +44,8 @@ public class TextAreaDialog extends StandardDialog {
 		createGUI(textAreaScrollPane, createButton("Close",e->closeDialog()));
 	}
 	
-	static void showText(Window parent, String title, int width, int height, String text) {
-		TextAreaDialog dlg = new TextAreaDialog(parent, title, ModalityType.APPLICATION_MODAL, false, width, height, false);
+	public static void showText(Window parent, String title, int width, int height, boolean textAreaLineWrap, String text) {
+		TextAreaDialog dlg = new TextAreaDialog(parent, title, ModalityType.APPLICATION_MODAL, false, width, height, false, textAreaLineWrap);
 		dlg.setText(text);
 		dlg.showDialog();
 	}
@@ -39,9 +58,15 @@ public class TextAreaDialog extends StandardDialog {
 		return textArea.getText();
 	}
 
-	private JButton createButton(String title, ActionListener al) {
+	private static JButton createButton(String title, ActionListener al) {
 		JButton comp = new JButton(title);
 		if (al!=null) comp.addActionListener(al);
+		return comp;
+	}
+
+	private static JCheckBoxMenuItem createCheckBoxMenuItem(String title, boolean isChecked, Consumer<Boolean> setValue) {
+		JCheckBoxMenuItem comp = new JCheckBoxMenuItem(title,isChecked);
+		if (setValue!=null) comp.addActionListener(e->setValue.accept(comp.isSelected()));
 		return comp;
 	}
 }
