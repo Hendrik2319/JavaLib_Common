5865dafdbcb090df3009531a244b476ca0c460b7
Hendrik Scholtz <hendrik.github@nurfuerspam.de>
2021-10-14 01:00:36 +0000

    JAVA: [Eclipse] - Projekt "CommonLib"
    git-svn-id: file:///G/_svn_repo/programming/JAVA-Projects%20-%20Eclipse/CommonLib@6044 8592820e-bdd2-cc16-cb4b-ca06df7e8abd

--- src/net/schwarzbaer/gui/StyledDocumentInterface.java
+++ src/net/schwarzbaer/gui/StyledDocumentInterface.java
@@ -24,6 +24,14 @@ public class StyledDocumentInterface {
 		subStyles = new HashMap<>();
 	}
 	
+	public void append(String format, Object... args) {
+		append((Style) null, format, args);
+	}
+	
+	public void append(Color color, String format, Object... args) {
+		append(new Style(color), format, args);
+	}
+	
 	public void append(Style style, String format, Object... args) {
 		append(getOrCreate(style), String.format(format, args));
 	}
@@ -61,27 +69,42 @@ public class StyledDocumentInterface {
 		public final Color color;
 		public final boolean isBold;
 		public final boolean isItalic;
+		public final String fontFamily;
 		
 		public Style(Color color) {
-			this(color, false, false);
+			this(color, false, false, null);
 		}
 		public Style(boolean isBold, boolean isItalic) {
-			this(null, isBold, isItalic);
+			this(null, isBold, isItalic, null);
 		}
 		public Style(Color color, boolean isBold, boolean isItalic) {
+			this(null, isBold, isItalic, null);
+		}
+		public Style(String fontFamily) {
+			this(null, false, false, fontFamily);
+		}
+		public Style(Color color, String fontFamily) {
+			this(color, false, false, fontFamily);
+		}
+		public Style(boolean isBold, boolean isItalic, String fontFamily) {
+			this(null, isBold, isItalic, fontFamily);
+		}
+		public Style(Color color, boolean isBold, boolean isItalic, String fontFamily) {
 			this.color = color;
 			this.isBold = isBold;
 			this.isItalic = isItalic;
+			this.fontFamily = fontFamily;
 		}
 		
 		public String getID() {
-			return String.format("%s:%s:%s", color==null ? "--------" : String.format("%08X", color.getRGB()), isBold ? "B" : "-", isItalic ? "I" : "-");
+			return String.format("%s:%s:%s:%s", color==null ? "--------" : String.format("%08X", color.getRGB()), isBold ? "B" : "-", isItalic ? "I" : "-", fontFamily==null ? "-----" : fontFamily);
 		}
 		
 		public void setValuesTo(javax.swing.text.Style subStyle) {
 			StyleConstants.setBold  (subStyle, isBold);
 			StyleConstants.setItalic(subStyle, isItalic);
-			if (color!=null) StyleConstants.setForeground(subStyle, color);
+			if (color     !=null) StyleConstants.setForeground(subStyle, color);
+			if (fontFamily!=null) StyleConstants.setFontFamily(subStyle, fontFamily);
 		}
 		
 		@Override
@@ -90,17 +113,20 @@ public class StyledDocumentInterface {
 			if (isBold  ) hashCode ^= 0xF0F0F0F0;
 			if (isItalic) hashCode ^= 0x0F0F0F0F;
 			if (color!=null) hashCode ^= color.getRGB();
+			if (fontFamily!=null) hashCode ^= fontFamily.hashCode();
 			return hashCode;
 		}
 		@Override
 		public boolean equals(Object obj) {
 			if (!(obj instanceof Style)) return false;
 			Style other = (Style) obj;
-			if (other.isBold!=this.isBold) return false;
-			if (other.isItalic!=this.isItalic) return false;
-			if (other.color==null && this.color==null) return true;
-			if (other.color==null || this.color==null) return false;
-			return other.color.getRGB()==this.color.getRGB();
+			if (other.isBold   != this.isBold  ) return false;
+			if (other.isItalic != this.isItalic) return false;
+			if ( (other.color==null) != (this.color==null) ) return false;
+			if (other.color!=null && this.color!=null && other.color.getRGB()!=this.color.getRGB()) return false;
+			if ( (other.fontFamily==null) != (this.fontFamily==null) ) return false;
+			if (other.fontFamily!=null && this.fontFamily!=null && !other.fontFamily.equals(this.fontFamily)) return false;
+			return true;
 		}
 		
 	}
