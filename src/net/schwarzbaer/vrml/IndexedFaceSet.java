70089c6b4fc7a11fd008bc06c2f34cad6129bfd7
Hendrik Scholtz <hendrik.github@nurfuerspam.de>
2021-11-14 13:49:39 +0000

    JAVA: [Eclipse] - Projekt "CommonLib"
    git-svn-id: file:///G/_svn_repo/programming/JAVA-Projects%20-%20Eclipse/CommonLib@6169 8592820e-bdd2-cc16-cb4b-ca06df7e8abd

--- src/net/schwarzbaer/vrml/IndexedFaceSet.java
+++ src/net/schwarzbaer/vrml/IndexedFaceSet.java
@@ -7,43 +7,36 @@ import java.util.Vector;
 
 import net.schwarzbaer.geometry.spacial.ConstPoint3d;
 
-public class IndexedLineSet extends PointBasedSet {
+public class IndexedFaceSet extends PointBasedSet {
 	
-	private final Vector<Integer> lines;
+	private final Vector<Integer> faces;
 
-	public IndexedLineSet(String pointCoordFormat, boolean optimizePointSet) {
+	public IndexedFaceSet(String pointCoordFormat, boolean optimizePointSet) {
 		super(pointCoordFormat, optimizePointSet);
-		lines = new Vector<>();
+		faces = new Vector<>();
 	}
-	
-	public void addAxesCross(ConstPoint3d p, double d) {
-		addLine(p.add( d,0,0),p.add(-d,0,0));
-		addLine(p.add(0, d,0),p.add(0,-d,0));
-		addLine(p.add(0,0, d),p.add(0,0,-d));
-	}
-	
-	public void addLine(ConstPoint3d... points) {
+
+	public void addFace(ConstPoint3d... points) {
 		for (ConstPoint3d p : points) {
 			int pIndex = addPoint(p);
-			lines.add(pIndex);
+			faces.add(pIndex);
 		}
-		lines.add(-1);
+		faces.add(-1);
 	}
-	
-	public void addLine(int... indexes) {
+	public void addFace(int... indexes) {
 		for (int i : indexes)
-			lines.add(i);
-		lines.add(-1);
+			faces.add(i);
+		faces.add(-1);
 	}
 	
-	public void writeToVRML(PrintWriter out, Color lineColor) { writeToVRML(out, lineColor, 0); }
 	public void writeToVRML(PrintWriter out, Color lineColor, double transparency) {
-		Iterable<String> it = ()->lines.stream().map(i->i.toString()).iterator();
+		Iterable<String> it = ()->faces.stream().map(i->i.toString()).iterator();
 		out.println("Shape {");
 		out.printf (Locale.ENGLISH,
 					"	appearance Appearance { material Material { emissiveColor %1.3f %1.3f %1.3f transparency %1.3f } }%n",
 					lineColor.getRed()/255f, lineColor.getGreen()/255f, lineColor.getBlue()/255f, transparency);
-		out.println("	geometry IndexedLineSet {");
+		out.println("	geometry IndexedFaceSet {");
+		out.println("		solid FALSE");
 		out.println("		coord Coordinate { point [ "+String.join(", ", points)+" ] }");
 		out.println("		coordIndex [ "+String.join(" ", it)+" ]");
 		out.println("	}");
