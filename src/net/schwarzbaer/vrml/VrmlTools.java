bb0fba17b01c37415531e37c8ac416376f1564c2
Hendrik Scholtz <hendrik.github@nurfuerspam.de>
2021-11-14 12:45:15 +0000

    JAVA: [Eclipse] - Projekt "CommonLib"
    git-svn-id: file:///G/_svn_repo/programming/JAVA-Projects%20-%20Eclipse/CommonLib@6164 8592820e-bdd2-cc16-cb4b-ca06df7e8abd

---
+++ src/net/schwarzbaer/vrml/VrmlTools.java
@@ -0,0 +1,35 @@
+package net.schwarzbaer.vrml;
+
+import java.awt.Color;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.Locale;
+import java.util.function.Consumer;
+
+public class VrmlTools {
+	
+	public static void writeVRML(File file, Consumer<PrintWriter> writeContent) {
+		try (PrintWriter out = new PrintWriter(new OutputStreamWriter(new FileOutputStream(file), StandardCharsets.UTF_8))) {
+			
+			writeHeader(out);
+			writeContent.accept(out);
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void writeHeader(PrintWriter out) { writeHeader(out, 0.6, 0.7, 0.8); }
+	public static void writeHeader(PrintWriter out, Color sky) { writeHeader(out, sky.getRed()/255f, sky.getGreen()/255f, sky.getBlue()/255f); }
+	public static void writeHeader(PrintWriter out, double skyR, double skyG, double skyB) {
+		out.println("#VRML V2.0 utf8");
+		out.println();
+		out.printf (Locale.ENGLISH, "Background { skyColor %1.3f %1.3f %1.3f }%n", skyR, skyG, skyB);
+		out.println();
+	}
+	
+}
