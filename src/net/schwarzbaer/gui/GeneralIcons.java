09a8ecf5fa5918f19388ff9fab933cb8bd655477
Hendrik Scholtz <hendrik.github@nurfuerspam.de>
2022-09-11 11:35:47 +0000

    JAVA: [Eclipse] - Projekt "CommonLib"
    git-svn-id: file:///G/_svn_repo/programming/JAVA-Projects%20-%20Eclipse/CommonLib@6691 8592820e-bdd2-cc16-cb4b-ca06df7e8abd

--- src/net/schwarzbaer/gui/GeneralIcons.java
+++ src/net/schwarzbaer/gui/GeneralIcons.java
@@ -5,11 +5,11 @@ import javax.swing.Icon;
 public final class GeneralIcons
 {
 	public enum GrayCommandIcons {
-		Muted, UnMuted, Up, Down, Power_IsOn, Power_IsOff, Reload, Download, Image, Save,
-		Muted_Dis, UnMuted_Dis, Up_Dis, Down_Dis, Power_IsOn_Dis, Power_IsOff_Dis, Reload_Dis, Download_Dis, Image_Dis, Save_Dis,
+		Muted, UnMuted, Up, Down, Power_IsOn, Power_IsOff, Reload, Download, Image, Save, Copy, Paste, Cut, Delete, Folder, AddFolder, ReloadFolder,
+		Muted_Dis, UnMuted_Dis, Up_Dis, Down_Dis, Power_IsOn_Dis, Power_IsOff_Dis, Reload_Dis, Download_Dis, Image_Dis, Save_Dis, Copy_Dis, Paste_Dis, Cut_Dis, Delete_Dis, Folder_Dis, AddFolder_Dis, ReloadFolder_Dis,
 		;
 		public Icon getIcon() { return iconSource.getCachedIcon(this); }
-		private static IconSource.CachedIcons<GrayCommandIcons> iconSource = IconSource.createCachedIcons(16, 16, 10, "GeneralIcons.GrayCommandIcons.png", GrayCommandIcons.values());
+		private static IconSource.CachedIcons<GrayCommandIcons> iconSource = IconSource.createCachedIcons(16, 16, 17, "GeneralIcons.GrayCommandIcons.png", GrayCommandIcons.values());
 	}
 	
 }
