785910fa7cdcb1158107d9e1a0ad936be61d555c
Hendrik Scholtz <hendrik.github@nurfuerspam.de>
2022-09-29 21:33:53 +0000

    JAVA: [Eclipse] - Projekt "CommonLib"
    git-svn-id: file:///G/_svn_repo/programming/JAVA-Projects%20-%20Eclipse/CommonLib@6785 8592820e-bdd2-cc16-cb4b-ca06df7e8abd

--- src/net/schwarzbaer/gui/StyledDocumentInterface.java
+++ src/net/schwarzbaer/gui/StyledDocumentInterface.java
@@ -28,14 +28,27 @@ public class StyledDocumentInterface {
 		append((Style) null, format, args);
 	}
 	
-	public void append(Color color, String format, Object... args) {
-		append(new Style(color), format, args);
+	public void append(Color textColor, String format, Object... args) {
+		append(new Style(textColor), format, args);
+	}
+	
+	public void append(Color textColor, Color backgroundColor, String format, Object... args) {
+		append(new Style(textColor, backgroundColor), format, args);
 	}
 	
 	public void append(Style style, String format, Object... args) {
 		append(getOrCreate(style), String.format(format, args));
 	}
 
+	private void append(javax.swing.text.Style style, String text) {
+		try {
+			doc.insertString(doc.getLength(), text, style); }
+		catch (BadLocationException e) {
+			System.err.printf("[StyledDocumentInterface] BadLocationException while inserting Strings into StyledDocument: %s%n", e.getMessage());
+			//e.printStackTrace();
+		}
+	}
+	
 	private javax.swing.text.Style getOrCreate(Style style) {
 		if (style==null)
 			return mainStyle;
@@ -52,13 +65,28 @@ public class StyledDocumentInterface {
 		
 		return docStyle;
 	}
-	
-	private void append(javax.swing.text.Style style, String text) {
-		try {
-			doc.insertString(doc.getLength(), text, style); }
+
+	public void changeStyle(Color textColor, int offset, int length)
+	{
+		changeStyle(new Style(textColor), offset, length);
+	}
+
+	public void changeStyle(Color textColor, Color backgroundColor, int offset, int length)
+	{
+		changeStyle(new Style(textColor, backgroundColor), offset, length);
+	}
+
+	public void changeStyle(Style style, int offset, int length)
+	{
+		doc.setCharacterAttributes(offset, length, getOrCreate(style), true);
+	}
+
+	public void clear()
+	{
+		try { doc.remove(0, doc.getLength()); }
 		catch (BadLocationException e) {
-			System.err.printf("[ValueListOutput.StyledDocumentOutput] BadLocationException while inserting Strings into StyledDocument: %s%n", e.getMessage());
-			//e.printStackTrace();
+			System.err.printf("[StyledDocumentInterface] BadLocationException while clearing content of StyledDocument: %s%n", e.getMessage());
+			e.printStackTrace();
 		}
 	}
 	
@@ -66,45 +94,44 @@ public class StyledDocumentInterface {
 		public static final Style BOLD   = new Style(true, false);
 		public static final Style ITALIC = new Style(false, true);
 		
-		public final Color color;
+		public final Color textColor;
+		public final Color backgroundColor;
 		public final boolean isBold;
 		public final boolean isItalic;
 		public final String fontFamily;
 		
-		public Style(Color color) {
-			this(color, false, false, null);
-		}
-		public Style(boolean isBold, boolean isItalic) {
-			this(null, isBold, isItalic, null);
-		}
-		public Style(Color color, boolean isBold, boolean isItalic) {
-			this(null, isBold, isItalic, null);
-		}
-		public Style(String fontFamily) {
-			this(null, false, false, fontFamily);
-		}
-		public Style(Color color, String fontFamily) {
-			this(color, false, false, fontFamily);
-		}
-		public Style(boolean isBold, boolean isItalic, String fontFamily) {
-			this(null, isBold, isItalic, fontFamily);
-		}
-		public Style(Color color, boolean isBold, boolean isItalic, String fontFamily) {
-			this.color = color;
+		public Style(Color textColor                                                     ) { this(textColor, null, false , false   , null      ); }
+		public Style(                 boolean isBold, boolean isItalic                   ) { this(null     , null, isBold, isItalic, null      ); }
+		public Style(Color textColor, boolean isBold, boolean isItalic                   ) { this(textColor, null, isBold, isItalic, null      ); }
+		public Style(                                                   String fontFamily) { this(null     , null, false , false   , fontFamily); }
+		public Style(Color textColor,                                   String fontFamily) { this(textColor, null, false , false   , fontFamily); }
+		public Style(                 boolean isBold, boolean isItalic, String fontFamily) { this(null     , null, isBold, isItalic, fontFamily); }
+		public Style(Color textColor, boolean isBold, boolean isItalic, String fontFamily) { this(textColor, null, isBold, isItalic, fontFamily); }
+		public Style(Color textColor, Color backgroundColor                              ) { this(textColor, backgroundColor, false, false, null); }
+		
+		public Style(Color textColor, Color backgroundColor, boolean isBold, boolean isItalic, String fontFamily) {
+			this.textColor = textColor;
+			this.backgroundColor = backgroundColor;
 			this.isBold = isBold;
 			this.isItalic = isItalic;
 			this.fontFamily = fontFamily;
 		}
 		
 		public String getID() {
-			return String.format("%s:%s:%s:%s", color==null ? "--------" : String.format("%08X", color.getRGB()), isBold ? "B" : "-", isItalic ? "I" : "-", fontFamily==null ? "-----" : fontFamily);
+			return String.format("%s:%s::%s:%s:%s",
+					textColor      ==null ? "--------" : String.format("%08X", textColor      .getRGB()),
+					backgroundColor==null ? "--------" : String.format("%08X", backgroundColor.getRGB()),
+					isBold   ? "B" : "-",
+					isItalic ? "I" : "-",
+					fontFamily==null ? "-----" : fontFamily);
 		}
 		
 		public void setValuesTo(javax.swing.text.Style subStyle) {
 			StyleConstants.setBold  (subStyle, isBold);
 			StyleConstants.setItalic(subStyle, isItalic);
-			if (color     !=null) StyleConstants.setForeground(subStyle, color);
-			if (fontFamily!=null) StyleConstants.setFontFamily(subStyle, fontFamily);
+			if (textColor      !=null) StyleConstants.setForeground(subStyle, textColor);
+			if (backgroundColor!=null) StyleConstants.setBackground(subStyle, backgroundColor);
+			if (fontFamily     !=null) StyleConstants.setFontFamily(subStyle, fontFamily);
 		}
 		
 		@Override
@@ -112,7 +139,8 @@ public class StyledDocumentInterface {
 			int hashCode = 0;
 			if (isBold  ) hashCode ^= 0xF0F0F0F0;
 			if (isItalic) hashCode ^= 0x0F0F0F0F;
-			if (color!=null) hashCode ^= color.getRGB();
+			if (textColor!=null) hashCode ^= textColor.getRGB();
+			if (backgroundColor!=null) hashCode ^= backgroundColor.getRGB();
 			if (fontFamily!=null) hashCode ^= fontFamily.hashCode();
 			return hashCode;
 		}
@@ -122,10 +150,12 @@ public class StyledDocumentInterface {
 			Style other = (Style) obj;
 			if (other.isBold   != this.isBold  ) return false;
 			if (other.isItalic != this.isItalic) return false;
-			if ( (other.color==null) != (this.color==null) ) return false;
-			if (other.color!=null && this.color!=null && other.color.getRGB()!=this.color.getRGB()) return false;
+			if ( (other.textColor==null) != (this.textColor==null) ) return false;
+			if (  other.textColor!=null  &&  this.textColor!=null && other.textColor.getRGB()!=this.textColor.getRGB()) return false;
+			if ( (other.backgroundColor==null) != (this.backgroundColor==null) ) return false;
+			if (  other.backgroundColor!=null  &&  this.backgroundColor!=null && other.backgroundColor.getRGB()!=this.backgroundColor.getRGB()) return false;
 			if ( (other.fontFamily==null) != (this.fontFamily==null) ) return false;
-			if (other.fontFamily!=null && this.fontFamily!=null && !other.fontFamily.equals(this.fontFamily)) return false;
+			if (  other.fontFamily!=null  &&  this.fontFamily!=null && !other.fontFamily.equals(this.fontFamily)) return false;
 			return true;
 		}
 		
