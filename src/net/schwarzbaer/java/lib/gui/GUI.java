6ee50ef49c1cd0e3aa4bb639fada56dd8b3c59d7
Hendrik Scholtz <hendrik.github@nurfuerspam.de>
2023-06-05 13:33:05 +0000

    JAVA: [Eclipse] - Projekt "CommonLib"
    git-svn-id: file:///G/_svn_repo/programming/JAVA-Projects%20-%20Eclipse/CommonLib@7198 8592820e-bdd2-cc16-cb4b-ca06df7e8abd

--- src/net/schwarzbaer/java/lib/gui/GUI.java
+++ src/net/schwarzbaer/java/lib/gui/GUI.java
@@ -24,8 +24,10 @@ import java.awt.geom.Rectangle2D;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.Vector;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -64,8 +66,6 @@ import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.filechooser.FileSystemView;
-
-import net.schwarzbaer.java.lib.system.Delayer;
 
 /**
  * 
@@ -913,4 +913,151 @@ public final class GUI {
 		return new JFrame(gc);
 	}
 
+	public static class Delayer implements Runnable {
+		
+		private Runnable task;
+		private long taskTime;
+		private boolean running;
+
+		public Delayer(Runnable task) {
+			this.task = task;
+			taskTime = 0;
+			running = false;
+		}
+		
+		public void delayTask( long ms ) {
+			//System.out.println("Delayer (re)set to "+ms+" miliseconds");
+			taskTime = System.currentTimeMillis()+ms;
+			if (!running) new Thread(this).start();
+		}
+		
+		@Override
+		public void run() {
+			//System.out.println("Delayer started");
+			running = true;
+			synchronized (this) {
+				while (true) {
+					long currentTime = System.currentTimeMillis();
+					if (currentTime>=taskTime) { 
+						task.run();
+						break;
+					} else {
+						try { wait(taskTime - currentTime); } catch (InterruptedException e) {}
+					}
+				}
+			}
+			running = false;
+			//System.out.println("Delayer ended");
+		}
+
+	}
+	
+	public static class Disabler<ActionCommands> {
+		
+		private HashMap<ActionCommands, Vector<JComponent>> map;
+
+		public Disabler() {
+			map = new HashMap<ActionCommands,Vector<JComponent>>();
+		}
+		
+		public void setCareFor(ActionCommands actionCommand) {
+			map.put(actionCommand,new Vector<JComponent>());
+		}
+
+		public void setCareFor(ActionCommands[] values) {
+			for (ActionCommands ac:values) setCareFor(ac);
+		}
+
+		public boolean caresFor(ActionCommands actionCommand) {
+			return map.containsKey(actionCommand);
+		}
+
+		public void showMap() {
+			System.out.println("Disabler.map:");
+			for (ActionCommands key:map.keySet()) {
+				System.out.printf("   key[%s]\r\n",key);
+				Vector<JComponent> list = map.get(key);
+				if (list==null)
+					System.out.printf("      <no entries>\r\n");
+				else
+					for (JComponent comp:list)
+						System.out.printf("      %s\r\n",comp);
+			}
+		}
+
+		public void addAll(ActionCommands actionCommand, JComponent... comps) {
+			for (JComponent comp:comps)
+				add(actionCommand,comp);
+		}
+
+		public JComponent add(ActionCommands actionCommand, JComponent comp) {
+			Vector<JComponent> list = map.get(actionCommand);
+			if (list==null) throw new UnsupportedOperationException("Disabler: Can't add components for unregistered ActionCommand "+actionCommand+". Please register it with disabler.setCareFor.");
+			list.add(comp);
+			return comp;
+		}
+
+		public void setEnableAll(boolean enabled) {
+			for (ActionCommands key:map.keySet())
+				setEnable(key, enabled);
+		}
+
+		public void setEnableAll( boolean enabled, ActionCommands exceptThis ) {
+			for (ActionCommands key:map.keySet())
+				if (!exceptThis.equals(key))
+					setEnable(key, enabled);
+		}
+
+		private boolean contains( ActionCommands[] values, ActionCommands value ) {
+			for(ActionCommands ac:values) if (ac.equals(value)) return true;
+			return false;
+		}
+		
+		public void setEnableAll( boolean enabled, ActionCommands[] exceptThis ) {
+			for (ActionCommands key:map.keySet()) 
+				if (!contains(exceptThis,key))
+					setEnable(key, enabled);
+		}
+
+		public void setEnableAll( boolean enabled, Vector<ActionCommands> exceptThis ) {
+			for (ActionCommands key:map.keySet())
+				if (!exceptThis.contains(key))
+					setEnable(key, enabled);
+		}
+
+		public void setEnable(Function<ActionCommands,Boolean> shouldEnable) {
+			for (ActionCommands key:map.keySet()) {
+				Boolean enabled = shouldEnable.apply(key);
+				if (enabled!=null) setEnable(key, enabled);
+			}
+		}
+
+		public void setEnable(ActionCommands[] actionCommands, boolean enabled) {
+			for (ActionCommands ac:actionCommands)
+				setEnable(ac, enabled);
+		}
+
+		public void setEnable(ActionCommands actionCommand, boolean enabled) {
+			Vector<JComponent> list = map.get(actionCommand);
+			if (list==null) throw new UnsupportedOperationException("Disabler: Can't use method setEnable on ActionCommand "+actionCommand+".");
+			for (JComponent c:list)
+				c.setEnabled(enabled);
+		}
+
+		public void configureAbstractButton(ActionCommands actionCommand, Consumer<AbstractButton> configure) {
+			Vector<JComponent> list = map.get(actionCommand);
+			if (list==null) throw new UnsupportedOperationException("Disabler: Can't use method configureAbstractButton on ActionCommand "+actionCommand+".");
+			for (JComponent c:list)
+				if (c instanceof AbstractButton)
+					configure.accept((AbstractButton) c);
+		}
+
+		public Set<ActionCommands> keySet() {
+			return map.keySet();
+		}
+		
+		public Vector<JComponent> get(ActionCommands key) {
+			return map.get(key);
+		}
+	}
 }
